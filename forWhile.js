27ee2e5f9532339ded1e5da6cf555a2df0335b3d
JAYADY MANAGAM GULTOM <jayadigultom@gmail.com>
2024-09-05 21:16:02 +0700

    commit pertama

---
+++ forWhile.js
@@ -0,0 +1,10 @@
+var jmlAngkot = 10;
+var angkotBeropeasi = 6;
+var noAngkot = 1;
+while(noAngkot <= angkotBeropeasi){
+    console.log("Angkot No." + noAngkot + " beroperasi dengan baik");
+noAngkot++;
+}
+for(noAngkot = 7; noAngkot <= jmlAngkot; noAngkot++){
+    console.log("Angkot No." + noAngkot + " sedang tidak beroperasi")
+}
